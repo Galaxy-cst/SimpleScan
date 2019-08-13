d46a18c7287dee57e535b00ad3fa8786968e41c6
Galaxy-cst <galaxy@gltlab.cn>
2019-08-13 19:46:19 +0800

    code format

--- app/Crawl/spider.py
+++ app/Crawl/spider.py
@@ -1,26 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+
+
 def find_all_links():
-    links=[]
+    links = []
     r = requests.get('https://www.baidu.com')
-    soup = BeautifulSoup(r.text)
+    soup = BeautifulSoup(r.text, "html.parser")
     for link in soup.find_all('a'):
         links.append(link.get('href'))
-        # print(link.get('href'))
-    print(links)
+    return links
+
 
 def find_all_forms():
-    forms=[]
+    forms = []
     r = requests.get('https://www.gltlab.cn')
-    soup = BeautifulSoup(r.text)
+    soup = BeautifulSoup(r.text, "html.parser")
     for form in soup.find_all('form'):
         action = form.action
-        method = form.method
         if hasattr(form, 'method'):
-            forms.append([action,method])
+            forms.append([form.get('action'), form.get('method')])
         else:
-            form.append([action])
-    print(forms)
-# find_all_links()
-find_all_forms()
-
+            forms.append([action])
+    return forms
